960358e461f07d2908dad9996d04ba6e0fac9b84
dtsiamasiotis <dtsiamasiotis@gmail.com>
2018-09-09 01:42:10 +0300

    add connect from the client

--- servlets/servlet/src/main/java/webSocketServer.java
+++ servlets/servlet/src/main/java/webSocketServer.java
@@ -1,3 +1,4 @@
+import javax.inject.Inject;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -7,14 +8,20 @@ import javax.websocket.server.*;
 @ServerEndpoint("/actions")
 public class webSocketServer {
 
+    @Inject
+    private backendManager backendManager;
+
     @OnOpen
     public void handleOpen(Session session)
     {
 
     }
     @OnMessage
-    public void handleMessage(String message)
+    public void handleMessage(Session session, String message)
     {
-        System.out.println(message);
+        if(message.contains("connectTruck"))
+            backendManager.connectFromTruck(session,message);
+        if(message.contains("test address"))
+            System.out.println(message);
     }
 }
