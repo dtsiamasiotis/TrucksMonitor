abc777392239df8f54e3add89f8b96c36fde9e18
dtsiamasiotis <dtsiamasiotis@gmail.com>
2019-08-06 13:30:51 +0300

    implement the necessary methods

--- servlets/servlet/src/main/java/messageEncoder.java
+++ servlets/servlet/src/main/java/messageEncoder.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import com.google.gson.Gson;
 
-public class messageEncoder {
+import javax.websocket.EncodeException;
+import javax.websocket.Encoder;
+import javax.websocket.EndpointConfig;
+
+public class messageEncoder implements Encoder.Text<message>{
+
+    private static Gson gson = new Gson();
+
+    @Override
+    public String encode(message message) throws EncodeException{
+        return gson.toJson(message);
+    }
+
+    @Override
+    public void init(EndpointConfig endpointConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
